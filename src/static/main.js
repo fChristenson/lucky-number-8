80488bb69b5919acec35c71a965031effde21102
Fredrik <fredrik.christenson@gmail.com>
2018-12-30 16:13:58 +0100

    finished algorithm

--- src/static/main.js
+++ src/static/main.js
@@ -1,3 +1,18 @@
 function randomNumber() {
   return Math.floor(Math.random() * 10);
 }
+
+const buttonElement = document.getElementById("button");
+const numberElement = document.getElementById("number");
+const winnerElement = document.getElementById("winner");
+
+buttonElement.addEventListener("click", e => {
+  const random = randomNumber();
+  number.textContent = random;
+
+  if (random === 8) {
+    winnerElement.style.display = "initial";
+  } else {
+    winnerElement.style.display = "none";
+  }
+});
