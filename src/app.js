419b6fffa7b45af9ba099d68a89638d4813e0f27
Fredrik <fredrik.christenson@gmail.com>
2018-12-30 15:58:10 +0100

    added random algorithm

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "static", "index.html"));
